c19b0c6bf210fa754d9ac3c93381abcaf4d9731d
Tsvetomir Milchev <tmilchev@progress.com>
2025-07-26 00:00:52 +0300

    feat(conf): define all composite filter router endpoints

--- src/configurations/src/routes/compositeFilterDescriptorRoutes.js
+++ src/configurations/src/routes/compositeFilterDescriptorRoutes.js
@@ -1,7 +1,42 @@
 import express from 'express';
+import compositeFilterDescriptorController from '../controllers/compositeFilterDescriptorController.js';
 
 const router = express.Router();
 
-const compositeFilterDescriptors = new Map();
+router.get(
+  '/composite-filters',
+  //   validateRequestBodyMiddleware(),
+  //   validateFilterDescriptorMiddleware(),
+  compositeFilterDescriptorController.getCompositeFilterDescriptors
+);
+
+router.get(
+  '/composite-filters/:id',
+  //   validateRequestBodyMiddleware(),
+  //   validateFilterDescriptorMiddleware(),
+  compositeFilterDescriptorController.getCompositeFilterDescriptorById
+);
+
+router.post(
+  '/composite-filters',
+  //   validateRequestBodyMiddleware(),
+  //   validateFilterDescriptorMiddleware(),
+  compositeFilterDescriptorController.createCompositeFilterDescriptor
+);
+
+router.put(
+  '/composite-filters/:id',
+  //   validateRequestBodyMiddleware(),
+  //   validateFilterDescriptorMiddleware(),
+  compositeFilterDescriptorController.updateCompositeFilterDescriptor
+);
+
+router.delete(
+  '/composite-filters/:id',
+  //   validateUUIDParamMiddleware(),
+  //   validateRequestBodyMiddleware(),
+  //   validateFilterDescriptorMiddleware(),
+  compositeFilterDescriptorController.deleteCompositeFilterDescriptor
+);
 
 export default router;
