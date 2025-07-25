66878032279855bc3f854a09f1c11625d62f2b29
Tsvetomir Milchev <tmilchev@progress.com>
2025-07-25 23:57:00 +0300

    feat(conf): define all filter router endpoints

--- src/configurations/src/routes/filterDescriptorRoutes.js
+++ src/configurations/src/routes/filterDescriptorRoutes.js
@@ -1,7 +1,39 @@
 import express from 'express';
-
+import filterdescriptorController from '../controllers/filterDescriptorController.js';
 const router = express.Router();
 
-const filterDescriptors = new Map();
+router.get(
+    '/filters',
+    //   validateRequestBodyMiddleware(),
+    //   validateFilterDescriptorMiddleware(),
+    filterdescriptorController.getFiltersDescriptors
+);
+
+router.get(
+    '/filters/:id',
+    //   validateRequestBodyMiddleware(),
+    //   validateFilterDescriptorMiddleware(),
+    filterdescriptorController.getFilterDescriptorById
+);
+
+router.post(
+    '/filters',
+    //   validateRequestBodyMiddleware(),
+    //   validateFilterDescriptorMiddleware(),
+    filterdescriptorController.createFilterDescriptor
+);
+
+router.put('/filters/:id',
+    //   validateRequestBodyMiddleware(),
+    //   validateFilterDescriptorMiddleware(),
+    filterdescriptorController.updateFilterDescriptor
+);
+
+router.delete('/filters/:id',
+    //   validateUUIDParamMiddleware(),
+    //   validateRequestBodyMiddleware(),
+    //   validateFilterDescriptorMiddleware(),
+    filterdescriptorController.deleteFilterDescriptor
+);
 
 export default router;
