3d1db8d7df588bb3fc3d7506f82729ab5e08d4f3
Tsvetomir Milchev <tmilchev@progress.com>
2025-07-25 15:58:03 +0300

    feat(fetcher): add block delay publisher

---
+++ src/fetcher/src/types/BlockDelayService.js
@@ -0,0 +1,21 @@
+import on from 'events';
+export class BlockDelayService {
+    blockProcessor;
+    delayBlocks;
+
+    constructor(blockProcessor, delayBlocks = 6) {
+        this.delayBlocks = delayBlocks;
+        this.blockProcessor = blockProcessor;
+
+        this.blockProcessor.on('blockProcessed', this.handleNewBlock.bind(this));
+        //this.blockProcessor.on('reorgDetected', this.handleReorg.bind(this));
+    }
+
+    destroy() {
+        this.blockProcessor.removeListener('blockProcessed', this.handleNewBlock.bind(this));
+    }
+
+    async handleNewBlock({ blockNumber, blockData }) {
+        console.log(`Processing new block: ${blockNumber}`);
+    }
+}
