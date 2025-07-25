1ce41ea0f3804ad77985393ca89fbace812bb8a7
Tsvetomir Milchev <tmilchev@progress.com>
2025-07-25 23:12:07 +0300

    feat(conf): add express router

--- src/configurations/src/app.js
+++ src/configurations/src/app.js
@@ -1,6 +1,19 @@
 import express from 'express';
+import filterDescriptorRoutes from './routes/filterDescriptorRoutes.js';
+import compositeFilterDescriptorRoutes from './routes/compositeFilterDescriptorRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use('/api', filterDescriptorRoutes);
+app.use('/api', compositeFilterDescriptorRoutes);
+
+app.use((req, res, next) => {
+    console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+    next();
+});
+
+app.listen(port, () => {
+    console.log(`\nConfiguration service listening on port ${port}!\n`);
+});
