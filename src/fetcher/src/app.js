985bf3260e05e96aa17e798a012036bbe12b9a71
Tsvetomir Milchev <tmilchev@progress.com>
2025-07-26 15:34:42 +0300

    playground(fetcher): add playground page

--- src/fetcher/src/app.js
+++ src/fetcher/src/app.js
@@ -1,25 +1,60 @@
 import dotenv from 'dotenv';
-import { InfuraWebClient } from './types/InfuraWebClient.js';
-import { BlockProcessor } from './types/BlockProcessor.js';
-
+import { InfuraWebClient } from './types/infuraWebClient.js';
+import { BlockProcessor } from './types/blockProcessor.js';
+import { BlockDelayService } from './services/blockDelayService.js';
+import { BlockStorageService } from './services/blockStorageService.js';
+import Redis from 'ioredis';
 dotenv.config();
+
+let testBlock = {
+    number: 1,
+    hash: "0x11111",
+}
+
+let testBlock2 = {
+    number: 2,
+    hash: "0x22222",
+    parentHash: "0x11111"    
+}
+
+let testBlock3 = {
+    number: 3,
+    hash: "0x33333",
+    parentHash: "0x22222"
+}
 
 console.log(process.env.INFURA_API_KEY);
 
 let url = `wss://mainnet.infura.io/ws/v3/${process.env.INFURA_API_KEY}`;
 
-let blockProcessor = new BlockProcessor();
+// Create separate Redis clients
+const redisConfig = {
+    host: process.env.REDIS_HOST || 'localhost',
+    port: process.env.REDIS_PORT || 6379
+};
 
-let client = new InfuraWebClient(url, blockProcessor);
+const redisClientForStorage = new Redis(redisConfig);
+const redisClientForDelay = new Redis(redisConfig);
 
-setTimeout(() => {
-    client.connect();
-}, 1000);
+let blockStorage = new BlockStorageService(redisClientForStorage);
+let blockProcessor = new BlockProcessor(blockStorage);
+let blockDelayService = new BlockDelayService(redisClientForDelay, 2);
+// blockDelayService.init();
+// let client = new InfuraWebClient(url, blockProcessor);
 
-setTimeout(() => {
-    client.disconnect();
-}, 15 * 1000);
+blockProcessor.processBlock(testBlock);
+blockProcessor.processBlock(testBlock2);
+blockProcessor.processBlock(testBlock3);
 
-setTimeout(() => {
-    console.log(client.blockProcessor.blocks);
-}, 15 * 1000);
+// setTimeout(() => {
+//     client.connect();
+// }, 1000);
+
+// setTimeout(() => {
+//     client.disconnect();
+// }, 15 * 1000);
+
+// setTimeout(() => {
+//     console.log("Exiting after 15 seconds...");
+//     process.exit(0);
+// }, 15 * 1000);
