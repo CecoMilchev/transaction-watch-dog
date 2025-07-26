2794ae16ccd20eee521f07cb6b45b27ae01a2b92
Tsvetomir Milchev <tmilchev@progress.com>
2025-07-26 11:37:05 +0300

    feat(fetcher): add storage service

---
+++ src/fetcher/src/services/blockStorageService.js
@@ -0,0 +1,37 @@
+export class BlockStorageService {
+    blocks = [];
+    maxBlocksToKeep = 100;
+
+    constructor() {
+    }
+
+    storeBlock(block) {
+        this.blocks.push(block);
+        
+        if (this.blocks.length > this.maxBlocksToKeep) {
+            this.blocks = this.blocks.slice(-this.maxBlocksToKeep);
+        }
+    }
+    
+    getBlock(blockNumber) {
+        return this.blocks.find(block => block.number === blockNumber);
+    }
+
+    getLastBlock() {
+        return this.blocks[this.blocks.length - 1] || null;
+    }
+
+    removeBlocksAfter(blockNumber) {
+        const initialLength = this.blocks.length;
+        this.blocks = this.blocks.filter(block => block.number <= blockNumber);
+        return initialLength - this.blocks.length;
+    }
+
+    getRecentBlocks(count = 10) {
+        return this.blocks.slice(-count);
+    }
+
+    getAllBlocks() {
+        return [...this.blocks];
+    }
+}
